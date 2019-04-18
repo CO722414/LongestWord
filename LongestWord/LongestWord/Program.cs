6c754747dfc069d3d3362718c2aa834913a6abcb
Ramandeep Kaur <722414@student.tor.loc>
2019-04-18 15:40:16 -0400

    longestword commit

--- LongestWord/LongestWord/Program.cs
+++ LongestWord/LongestWord/Program.cs
@@ -8,8 +8,53 @@ namespace LongestWord
 {
     class Program
     {
-        static void Main(string[] args)
-        {
+   
+            static void Main(string[] args)
+            {
+                Program a = new Program();
+                Console.WriteLine("the longest word is :");
+
+                Console.Write(a.MyFunctionA("Ada lovalace wrote the first algorithm designed for processing by an Anatytical Engine."));
+
+            }
+            public string MyFunctionA(string input)
+            {
+                //write a c#program to find the longest word in a string.
+                string[] words = input.Split(' ');
+
+                int wordArrayLength = words.Length;
+                int[] wordsLength = new int[wordArrayLength];
+                int x = 0;
+
+                foreach (var word in words)
+                {
+
+                }
+                //loop post condition:we now have array wordsLength which
+                // contains the lengths of each word
+
+                String currentWord = words[0];
+                String nextWord;
+                String longestWord = currentWord;
+
+                for (int y = 0; y < words.Length - 1; y++)
+                {
+
+                    currentWord = words[y];
+                    nextWord = words[y + 1];
+                    if (nextWord.Length > longestWord.Length)
+                    {
+                        longestWord = nextWord;
+                    }
+                    else
+                    {
+                        longestWord = currentWord;
+                    }
+
+                }
+                return longestWord;
+            }
+
         }
     }
-}
+
